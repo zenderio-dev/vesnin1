175b8db474ea3fabcd9c2b9a1a86863ac45c8237
Max <moximka06@gmail.com>
2026-02-10 14:28:00 +0800

    test

---
+++ test.py
@@ -0,0 +1,27 @@
+from script import sum ,devide
+def test_sum():
+    a = 1
+    b = 2
+    result = 3
+    assert sum(a,b) == result
+
+def test_devide():
+    a = 2
+    b = 4
+    result = 0.5
+    assert devide(a,b) == result
+
+def test_devide_zero():
+    a = 2
+    b = 0
+    try:
+        devide(a,b)
+        assert False
+    except ValueError as e:
+        print("Good")
+
+if name == "main":
+    test_sum()
+    test_devide()
+    test_devide_zero()
+    
