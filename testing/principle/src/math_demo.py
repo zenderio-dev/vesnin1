76dbb729e95cc73dcbbaff93f5a9c671a9cd2849
Max <moximka06@gmail.com>
2026-03-05 14:12:00 +0800

    moddificated calculate_tax

--- testing/principle/src/math_demo.py
+++ testing/principle/src/math_demo.py
@@ -8,4 +8,7 @@ def calculate_tax_bagged(income):
     return income * 0.15
 
 def calculate_tax(income):
+    if income < 0:
+        raise ValueError("Income cannot be negative")
     return int(income * 0.15 * 100) / 100
+
