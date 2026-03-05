d9ee34af0ed4c979df8776165fa83b75154cc368
Max <moximka06@gmail.com>
2026-03-05 12:58:31 +0800

    add add_with_bug and test for this

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -2,13 +2,21 @@
 #import sys
 #sys.path.append("../src")
 # TODO make it with pip install -e
-from math_demo import add
-
+from math_demo import add, add_with_bug
 
 
 def test_addition():
-    assert 2 + 2 == 4
+    assert add(2, 2) == 4
     print("Test ADDITION PASSED")
+
+def test_addition_with_bug():
+    assert add_with_bug(2, 2) == 4
+    assert add_with_bug(0, 0) == 0
+    print("Test BUGGED ADDITION PASSED")
+    # finally we found data that make test reliable
+    # assert add_with_bug(7, 6) == 13
+    
 
 if __name__ == "__main__":
     test_addition()
+    test_addition_with_bug()
