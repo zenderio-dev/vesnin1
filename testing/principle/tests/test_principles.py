e211fa102a85addde575e18522f6be09b8d1b68b
Max <moximka06@gmail.com>
2026-03-05 13:04:15 +0800

    add test dublicate

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -7,6 +7,8 @@ from math_demo import add, add_with_bug
 
 def test_addition():
     assert add(2, 2) == 4
+    assert add_with_bug(0, 0) == 0
+    assert add_with_bug(7, 6) == 13
     print("Test ADDITION PASSED")
 
 def test_addition_with_bug():
@@ -16,7 +18,11 @@ def test_addition_with_bug():
     # finally we found data that make test reliable
     # assert add_with_bug(7, 6) == 13
     
+def test_addition_dublicate():
+    assert add(6, 7) == 6 + 7
+    print("Test DUPLICATE ADDITION PASSED")
 
 if __name__ == "__main__":
     test_addition()
     test_addition_with_bug()
+    test_addition_dublicate()
